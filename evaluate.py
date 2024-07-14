31610a61e951036e0f097d72cd47c94cf7289660
Yoel <yoelashkenazi12@gmail.com>
2024-07-14 13:20:34 +0300

    initial tests with evaluation succeeded. further tests are needed

--- evaluate.py
+++ evaluate.py
@@ -4,9 +4,17 @@ This file is responsible for evaluating the summarization results using GPT-4 ap
 """
 import os
 import pandas as pd
-import openai
+from openai import OpenAI
 import random
 import pickle as pkl
+
+# organization='org-FKQBIvqIr7JF5Jhysdnrxx5z',
+api_key = 'sk-proj-2gkj2jzXE7fAgLzqIn9TT3BlbkFJzQwHpetTBkmbSO6Q6KBl'
+organization = 'org-FKQBIvqIr7JF5Jhysdnrxx5z'
+# set the openai api key. and the organization. also as an environment variable.
+os.environ['OPENAI_API_KEY'] = api_key
+
+client = OpenAI(api_key=api_key, organization=organization)
 
 
 def evaluate(name: str):
@@ -47,25 +55,25 @@ def evaluate(name: str):
 
         print(subgraphs[cluster].nodes(data=True))
 
-    # organization='org-FKQBIvqIr7JF5Jhysdnrxx5z',
-    api_key = 'sk-proj-vm0poBE6tqDcfG1OwGHhT3BlbkFJLiYmkIoWVrb2mj6RhaH1'
-    openai.api_key = api_key
-
     # for each summary and cluster pairs, sample abstracts from the cluster and outside the cluster.
     # then ask GPT which abstracts are more similar to the summary.
 
-    data = pd.read_csv(f"data/graphs/{name}_papers.csv")
+    data = pd.read_csv(f"data/graphs/{name}_papers.csv")[['id', 'abstract']]  # load the data.
     evaluations = {}
     for cluster, summary in summaries.items():
         # get the subgraph.
         subgraph = subgraphs[cluster]
+        cluster_name = cluster.split('_')[2]  # get the color.
 
         # get the abstracts from the cluster.
-        cluster_abstracts = [data['abstract'][node] for node in subgraph.nodes() if type(data['abstract'][node]) == str]
+        cluster_abstracts = [abstract for id_, abstract in data.values if id_ in subgraph.nodes()]
 
         # get the abstracts from outside the cluster.
-        outside_abstracts = [data['abstract'][node] for node in G.nodes() if node not in subgraph.nodes() and
-                             type(data['abstract'][node]) == str]
+        outside_abstracts = [abstract for id_, abstract in data.values if id_ not in subgraph.nodes()]
+
+        # clean nans.
+        cluster_abstracts = [abstract for abstract in cluster_abstracts if not pd.isna(abstract)]
+        outside_abstracts = [abstract for abstract in outside_abstracts if not pd.isna(abstract)]
 
         # sample 20% of the cluster size (k) and k abstracts from outside the cluster.
         cluster_abstracts = random.sample(cluster_abstracts, int(0.2 * len(cluster_abstracts)))
@@ -76,38 +84,51 @@ def evaluate(name: str):
         outside_avg_score = 0
         for i in range(len(cluster_abstracts)):
             # ask GPT which abstract is more similar to the summary.
-            response = openai.Completion.create(
-                engine="gpt-4",
-                prompt=f"How consistent is the following summary with the abstract from 1 to 100?\n"
-                       f"Summary: {summary}\n"
-                       f"Abstract: {cluster_abstracts[i]}\n",
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": f"Answer using only a number between 1 to 100: "
+                                   f"How consistent is the following summary with the abstract?\n"
+                                   f"Summary: {summary}\n"
+                                   f"Abstract: {cluster_abstracts[i]}\n"
+                    }
+                ],
                 max_tokens=100,
                 n=1,
                 stop=None,
                 temperature=0.5
             )
 
+            score_in = response.choices[0].message.content
             # get the score.
-            score = response['choices'][0]['score']
-
-            cluster_avg_score += score
+            score_in = int(score_in.split('\n')[-1].split(':')[-1])
+            cluster_avg_score += score_in
 
             # ask GPT which abstract is more similar to the summary.
-            response = openai.Completion.create(
-                engine="gpt-4",
-                prompt=f"How consistent is the following summary with the abstract from 1 to 100?\n"
-                       f"Summary: {summary}\n"
-                       f"Abstract: {outside_abstracts[i]}\n",
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": f"Answer using only a number between 1 to 100:"
+                                   f"How consistent is the following summary with the abstract?\n"
+                                   f"Summary: {summary}\n"
+                                   f"Abstract: {outside_abstracts[i]}\n"
+                    }
+                ],
                 max_tokens=100,
                 n=1,
                 stop=None,
                 temperature=0.5
             )
 
+            score_out = response.choices[0].message.content
             # get the score.
-            score = response['choices'][0]['score']
+            score_out = int(score_out.split('\n')[-1].split(':')[-1])
 
-            outside_avg_score += score
+            outside_avg_score += score_out
 
         # get the average scores.
         cluster_avg_score /= len(cluster_abstracts)
@@ -115,11 +136,16 @@ def evaluate(name: str):
 
         decision = "consistent" if cluster_avg_score >= outside_avg_score else "inconsistent"
 
-        evaluations[cluster] = (cluster_avg_score, outside_avg_score, decision)
+        evaluations[cluster_name] = (cluster_avg_score, outside_avg_score, decision)
 
-        print(f"Cluster summary for {cluster} is {decision} with the cluster abstracts. (score: {cluster_avg_score})")
+        print(f"Cluster summary for cluster '{cluster_name}' is {decision} with the cluster abstracts. "
+              f"\nScore in: {cluster_avg_score}\nScore out: {outside_avg_score}\n{'-' * 50}")
 
     df = pd.DataFrame(evaluations, index=['cluster_score', 'outside_score', 'decision'])
-    df.to_csv(f"Evaluations/{name}_evaluations.csv")
+    try:
+        df.to_csv(f"Evaluations/{name}_evaluations.csv")
+    except OSError:
+        os.makedirs("Evaluations/")
+        df.to_csv(f"Evaluations/{name}_evaluations.csv")
     print(f"Finished evaluating the summaries for {name}.")
     return df
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import functions
 import warnings
-import summarize
+# import summarize
 import evaluate
 
 warnings.filterwarnings("ignore")
